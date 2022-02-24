f2dc193609e138b77d27fb945c792e92d051144a
Vipul Chinnawar <vipulchinnawar@gmail.com>
2022-02-25 01:20:46 +0530

    New commit

---
+++ Phase-3/Lesson1/eventhandlingspring/src/main/java/com/eventhandlingspring/CustomEvent.java
@@ -0,0 +1,15 @@
+package com.eventhandlingspring;
+import org.springframework.context.ApplicationEvent;
+public class CustomEvent extends ApplicationEvent{
+
+		public CustomEvent(Object source) {
+			super(source);
+		}
+
+		@Override
+		public String toString() {
+			return "CustomEvent: [This is My Custom Event]";
+		}
+
+		
+	}
---
+++ Phase-3/Lesson1/eventhandlingspring/src/main/java/com/eventhandlingspring/CustomEventListener.java
@@ -0,0 +1,12 @@
+package com.eventhandlingspring;
+import org.springframework.context.ApplicationListener;
+
+public class CustomEventListener implements ApplicationListener<CustomEvent> {
+
+	public void onApplicationEvent(CustomEvent event) {
+		
+		System.out.println(event.toString());
+		
+	}
+
+}
