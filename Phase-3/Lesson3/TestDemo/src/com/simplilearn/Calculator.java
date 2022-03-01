35578a3d208d90c53a8589c91a39973639babcb4
Vipul Chinnawar <vipulchinnawar@gmail.com>
2022-03-02 02:40:10 +0530

    Phase3

---
+++ Phase-3/Lesson3/TestDemo/src/com/simplilearn/Calculator.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+public class Calculator {
+
+	int add(int a,int b)
+	{
+		return a+b;
+	}
+	
+	int sub(int a,int b)
+	{
+		return a-b;
+	}
+}
---
+++ Phase-3/Lesson3/TypesJUnitTest/src/main/java/com/simplilearn/TypesJUnitTest/Calculator.java
@@ -0,0 +1,8 @@
+package com.simplilearn.TypesJUnitTest;
+public class Calculator {
+
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+}
