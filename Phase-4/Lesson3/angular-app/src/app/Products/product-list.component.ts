6f1687c2b12a6d81281043e9aaccf94a8ee80e74
Vipul Chinnawar <vipulchinnawar@gmail.com>
2022-03-15 14:06:53 +0530

    phase-4 complete

---
+++ Phase-4/Lesson3/angular-app/src/app/Products/product-list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'product-list',
+    templateUrl:'./product-list.component.html'
+})
+
+export class ProductListComponent{
+    pageTitle: string ="product List page"
+}
